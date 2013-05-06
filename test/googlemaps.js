angular-ui/angular-ui-OLDREPO

commit d77bfe74e4ca2c463f76bac4f8b2e1e7464f7773

fix(map): Closed #390 - Changed elm.trigger() to elm.triggerHandler()

modules/directives/map/map.js
@@ -9,7 +9,7 @@
       //for the googlemap doesn't interfere with a normal 'click' event
       var $event = { type: 'map-' + eventName };
       google.maps.event.addListener(googleObject, eventName, function (evt) {
-        element.trigger(angular.extend({}, $event, evt));
+        element.triggerHandler(angular.extend({}, $event, evt));
         //We create an $apply if it isn't happening. we need better support for this
         //We don't want to use timeout because tons of these events fire at once,
         //and we only need one $apply

angular-ui/ui-map

commit 410951b960babcc2c6ab90ff78ce235eab90833a

fix(#3): jqLite.triggerHandler() doesn't accept event objects
- By passing an event string name instead events should resume firing
properly.
- Converted $() to angular.element
- Converted .trigger() to .triggerHandler() in spec
- Added 'ui.event' as a module dependency
- Re-enabled tests
- Regression introduced by d77bfe74e4ca2c463f76bac4f8b2e1e7464f7773

src/map.js
@@ -1,5 +1,5 @@
 (function () {
-  var app = angular.module('ui.map', []);
+  var app = angular.module('ui.map', ['ui.event']);
 
   //Setup map events from a google map object to trigger on a given element too,
   //then we just use ui-event to catch events from an element
@@ -7,9 +7,8 @@
     angular.forEach(eventsStr.split(' '), function (eventName) {
       //Prefix all googlemap events with 'map-', so eg 'click' 
       //for the googlemap doesn't interfere with a normal 'click' event
-      var $event = { type: 'map-' + eventName };
-      google.maps.event.addListener(googleObject, eventName, function (evt) {
-        element.triggerHandler(angular.extend({}, $event, evt));
+      google.maps.event.addListener(googleObject, eventName, function (event) {
+        element.triggerHandler('map-' + eventName, event);
         //We create an $apply if it isn't happening. we need better support for this
         //We don't want to use timeout because tons of these events fire at once,
         //and we only need one $apply
@@ -18,7 +17,7 @@
     });
   }
 
-  app.directive('uiMap',
+  app.value('uiMapConfig', {}).directive('uiMap',
     ['uiMapConfig', '$parse', function (uiMapConfig, $parse) {
 
       var mapEvents = 'bounds_changed center_changed click dblclick drag dragend ' +

test/googlemaps.js
@@ -1,0 +1,21 @@
+window.google = window.google || {};
+google.maps = google.maps || {};
+(function() {
+  
+  function getScript(src) {
+    document.write('<' + 'script src="' + src + '"' +
+                   ' type="text/javascript"><' + '/script>');
+  }
+  
+  var modules = google.maps.modules = {};
+  google.maps.__gjsload__ = function(name, text) {
+    modules[name] = text;
+  };
+  
+  google.maps.Load = function(apiLoad) {
+    delete google.maps.Load;
+    apiLoad([null,[[["http://mt0.googleapis.com/vt?lyrs=m@180000000\u0026src=api\u0026hl=en-US\u0026","http://mt1.googleapis.com/vt?lyrs=m@180000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"m@180000000"],[["http://khm0.googleapis.com/kh?v=115\u0026hl=en-US\u0026","http://khm1.googleapis.com/kh?v=115\u0026hl=en-US\u0026"],null,null,null,1,"115"],[["http://mt0.googleapis.com/vt?lyrs=h@180000000\u0026src=api\u0026hl=en-US\u0026","http://mt1.googleapis.com/vt?lyrs=h@180000000\u0026src=api\u0026hl=en-US\u0026"],null,null,"imgtp=png32\u0026",null,"h@180000000"],[["http://mt0.googleapis.com/vt?lyrs=t@129,r@180000000\u0026src=api\u0026hl=en-US\u0026","http://mt1.googleapis.com/vt?lyrs=t@129,r@180000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"t@129,r@180000000"],null,[[null,0,7,7,[[[330000000,1246050000],[386200000,1293600000]],[[366500000,1297000000],[386200000,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026"]],[null,0,8,8,[[[330000000,1246050000],[386200000,1279600000]],[[345000000,1279600000],[386200000,1286700000]],[[354690000,1286700000],[386200000,1320035000]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026"]],[null,0,9,9,[[[330000000,1246050000],[386200000,1279600000]],[[340000000,1279600000],[386200000,1286700000]],[[348900000,1286700000],[386200000,1302000000]],[[368300000,1302000000],[386200000,1320035000]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026"]],[null,0,10,19,[[[329890840,1246055600],[386930130,1284960940]],[[344646740,1284960940],[386930130,1288476560]],[[350277470,1288476560],[386930130,1310531620]],[[370277730,1310531620],[386930130,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1.16\u0026hl=en-US\u0026"]],[null,3,7,7,[[[330000000,1246050000],[386200000,1293600000]],[[366500000,1297000000],[386200000,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026"]],[null,3,8,8,[[[330000000,1246050000],[386200000,1279600000]],[[345000000,1279600000],[386200000,1286700000]],[[354690000,1286700000],[386200000,1320035000]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026"]],[null,3,9,9,[[[330000000,1246050000],[386200000,1279600000]],[[340000000,1279600000],[386200000,1286700000]],[[348900000,1286700000],[386200000,1302000000]],[[368300000,1302000000],[386200000,1320035000]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026"]],[null,3,10,null,[[[329890840,1246055600],[386930130,1284960940]],[[344646740,1284960940],[386930130,1288476560]],[[350277470,1288476560],[386930130,1310531620]],[[370277730,1310531620],[386930130,1320034790]]],["http://mt0.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026","http://mt1.gmaptiles.co.kr/mt?v=kr1p.16\u0026hl=en-US\u0026"]]],[["http://cbk0.googleapis.com/cbk?","http://cbk1.googleapis.com/cbk?"]],[["http://khm0.googleapis.com/kh?v=60\u0026hl=en-US\u0026","http://khm1.googleapis.com/kh?v=60\u0026hl=en-US\u0026"],null,null,null,null,"60"],[["http://mt0.googleapis.com/mapslt?hl=en-US\u0026","http://mt1.googleapis.com/mapslt?hl=en-US\u0026"]],[["http://mt0.googleapis.com/mapslt/ft?hl=en-US\u0026","http://mt1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["http://mt0.googleapis.com/vt?hl=en-US\u0026","http://mt1.googleapis.com/vt?hl=en-US\u0026"]]],["en-US","US",null,0,null,null,"http://maps.gstatic.com/mapfiles/","http://csi.gstatic.com","https://maps.googleapis.com","http://maps.googleapis.com"],["http://maps.gstatic.com/intl/en_us/mapfiles/api-3/9/12","3.9.12"],[4027454879],1.0,null,null,null,null,0,"",null,null,0,"http://khm.googleapis.com/mz?v=115\u0026",null,"https://earthbuilder.google.com","https://earthbuilder.googleapis.com"], loadScriptTime);
+  };
+  var loadScriptTime = (new Date).getTime();
+  getScript("http://maps.gstatic.com/intl/en_us/mapfiles/api-3/9/12/main.js");
+})();

test/mapSpec.js
@@ -1,4 +1,4 @@
-xdescribe('uiMap', function () {
+describe('uiMap', function () {
   var scope, $rootScope, $compile;
 
   beforeEach(module('ui.map'));
@@ -10,17 +10,17 @@ xdescribe('uiMap', function () {
   function createMap(options, events) {
     scope.gmapOptions = options || {};
     scope.gmapEvents = events || {};
-    $compile("<div ui-map='gmap' ui-options='gmapOptions'" +
-      "' ui-event='gmapEvents'></div>")(scope);
+    $compile("<div><div ui-map='gmap' ui-options='gmapOptions'" +
+      "' ui-event='gmapEvents'></div></div>")(scope);
   }
 
   function createWindow(options, events, inner) {
     scope.gOptions = options || {};
     scope.gEvents = events || {};
-    inner = inner || angular.element('');
-    var elm = angular.element("<div ui-map-info-window='ginfo' " +
-      "ui-options='gOptions' ui-event='gEvents'></div>");
-    elm.append(inner);
+    var elm = angular.element("<div><div ui-map-info-window='ginfo' " +
+      "ui-options='gOptions' ui-event='gEvents'></div></div>");
+    if (inner)
+      elm.children().append(inner);
     $compile(elm)(scope);
   }
 
@@ -67,15 +67,15 @@ xdescribe('uiMap', function () {
     });
 
     it('should create info window with given options & content', function () {
-      var content = $('<h1>Hi</h1>');
+      var content = angular.element('<h1>Hi</h1>');
       createWindow({ zIndex: 5 }, {}, content);
       expect(scope.ginfo.getZIndex()).toBe(5);
       expect(scope.ginfo.getContent().innerHTML)
-        .toBe($('<div>').append(content).html());
+        .toBe(angular.element('<div>').append(content).html());
     });
 
     it('should $compile content and recognize scope changes', function () {
-      var inner = $('<input ng-model="myVal">');
+      var inner = angular.element('<input ng-model="myVal">');
       createWindow({}, {}, inner);
       createMap();
       scope.$apply(function () {
@@ -90,6 +90,7 @@ xdescribe('uiMap', function () {
     });
 
     it('should recognize infowindow events in ui-event as "map-eventname"', function () {
+      expect(scope.closed).toBeUndefined();
       createWindow({}, {
         'map-closeclick': 'closed = true'
       });

test/test.conf.js
@@ -5,16 +5,18 @@ files = [
   JASMINE_ADAPTER,
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
+  'components/angular-ui-utils/modules/event/event.js',
   'src/*.js',
+  'test/googlemaps.js',
   'test/*Spec.js'
 ];
 
-// Avoid including minified version of angular and other libs again
-exclude = [
-  'components/*/*.min.js'
-];
-
 singleRun = true;
+
+// level of logging
+// possible values = karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
+// CLI --log-level debug
+logLevel = LOG_INFO,
 
 reporters = [
 	'dots'