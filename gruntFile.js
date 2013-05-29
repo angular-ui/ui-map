0eaa69876c895c1363d8730e0ce25a3a372f6f0e
Douglas Duteil <douglasduteil@gmail.com>
2013-05-29 15:28:04 +0200

    [M] Doc as bower module. close angular-ui/angular-ui.github.com#77

--- gruntFile.js
+++ gruntFile.js
@@ -17,6 +17,7 @@ module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    dist : 'components/angular-ui-docs',
     pkg: grunt.file.readJSON('package.json'),
     meta: {
       banner: ['/**',
@@ -53,17 +54,17 @@ module.exports = function (grunt) {
       options: {banner: '<%= meta.banner %>'},
       build: {
         files: {
-          'out/build/<%= meta.view.repoName %>.min.js': ['<%= meta.view.repoName %>.js']
+          '<%= dist %>/build/<%= meta.view.repoName %>.min.js': ['<%= meta.view.repoName %>.js']
         }
       }
     },
     copy: {
       main: {
         files: [
-          {src: ['<%= meta.view.repoName %>.js'], dest: 'out/build/<%= meta.view.repoName %>.js', filter: 'isFile'},
-          {src: ['demo/demo.html'], dest: 'out/demos.html', filter: 'isFile'},
-          {src: ['demo/demo.css'], dest: 'out/assets/css/demos.css', filter: 'isFile'},
-          {src: ['demo/demo.js'], dest: 'out/core/demo.js', filter: 'isFile'}
+          {src: ['<%= meta.view.repoName %>.js'], dest: '<%= dist %>/build/<%= meta.view.repoName %>.js', filter: 'isFile'},
+          {src: ['demo/demo.html'], dest: '<%= dist %>/demos.html', filter: 'isFile'},
+          {src: ['demo/demo.css'], dest: '<%= dist %>/assets/css/demos.css', filter: 'isFile'},
+          {src: ['demo/demo.js'], dest: '<%= dist %>/core/demo.js', filter: 'isFile'}
         ]
       },
       template : {
@@ -71,7 +72,7 @@ module.exports = function (grunt) {
           return grunt.template.process(content);
         }},
         files: [
-          {src: ['out/.tmpl/index.tmpl'], dest: 'out/index.html'}
+          {src: ['<%= dist %>/.tmpl/index.tmpl'], dest: '<%= dist %>/index.html'}
         ]
       }
     }
