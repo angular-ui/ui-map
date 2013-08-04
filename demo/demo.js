bb3a1ecdcbe66277d3a80cd8fc4a0d6301708d6b
Douglas Duteil <douglasduteil@gmail.com>
2013-08-04 20:45:24 +0200

    [M] Update to the new angular-ui/angular-ui-docs#8

--- demo/demo.js
+++ demo/demo.js
@@ -1,66 +1,40 @@
+function initCall() {
+  console.log("Google maps api initialized.");
+  angular.bootstrap(document.getElementById("map"), ['doc.ui-map']);
+}
 
-$("#map").hide();
-requireCss('assets/css/demos.css');
+angular.module('doc.ui-map', ['ui.map', 'prettifyDirective'])
+  .controller('MapCtrl', ['$scope', function ($scope) {
 
-function initCall(){ console.log("Google maps api initialized.");}
+    $scope.myMarkers = [];
 
-requirejs(
-  {
-    paths: {
-      'ui.map': "build/ui-map"
-    },
-    shim: {
-      'ui.map': { deps: [
-        'https://rawgithub.com/angular-ui/ui-utils/master/modules/event/event.js',
-        'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initCall'
-      ] }
-    }
-  },
-  ['ui.map'],
-  function () {
+    $scope.mapOptions = {
+      center: new google.maps.LatLng(35.784, -78.670),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-    angular.module('doc.ui-map', ['ui.map', 'prettifyDirective'])
-      .controller('MapCtrl', ['$scope', function ($scope) {
+    $scope.addMarker = function ($event) {
+      $scope.myMarkers.push(new google.maps.Marker({
+        map: $scope.myMap,
+        position: $event.latLng
+      }));
+    };
 
-        $scope.myMarkers = [];
+    $scope.setZoomMessage = function (zoom) {
+      $scope.zoomMessage = 'You just zoomed to ' + zoom + '!';
+      console.log(zoom, 'zoomed');
+    };
 
-        $scope.mapOptions = {
-          center: new google.maps.LatLng(35.784, -78.670),
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+    $scope.openMarkerInfo = function (marker) {
+      $scope.currentMarker = marker;
+      $scope.currentMarkerLat = marker.getPosition().lat();
+      $scope.currentMarkerLng = marker.getPosition().lng();
+      $scope.myInfoWindow.open($scope.myMap, marker);
+    };
 
-        $scope.addMarker = function($event) {
-          $scope.myMarkers.push(new google.maps.Marker({
-            map: $scope.myMap,
-            position: $event.latLng
-          }));
-        };
-
-        $scope.setZoomMessage = function(zoom) {
-          $scope.zoomMessage = 'You just zoomed to '+zoom+'!';
-          console.log(zoom,'zoomed');
-        };
-
-        $scope.openMarkerInfo = function(marker) {
-          $scope.currentMarker = marker;
-          $scope.currentMarkerLat = marker.getPosition().lat();
-          $scope.currentMarkerLng = marker.getPosition().lng();
-          $scope.myInfoWindow.open($scope.myMap, marker);
-        };
-
-        $scope.setMarkerPosition = function(marker, lat, lng) {
-          marker.setPosition(new google.maps.LatLng(lat, lng));
-        };
-      }])
-    ;
-    e$ = $("#map");
-    e$.removeAttr("ng-non-bindable");
-
-    angular.bootstrap(e$[0], ['doc.ui-map']);
-    e$.show();
-    $("#map-l").slideUp();
-
-
-  }
-);
+    $scope.setMarkerPosition = function (marker, lat, lng) {
+      marker.setPosition(new google.maps.LatLng(lat, lng));
+    };
+  }])
+;
--- gruntFile.js
+++ gruntFile.js
@@ -4,20 +4,24 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'karma']);
+  grunt.registerTask('default', ['jshint', 'karma:unit']);
   grunt.registerTask('build-doc', ['uglify', 'copy']);
+  grunt.registerTask('server', ['karma:start']);
 
-  var testConfig = function(configFile, customOptions) {
+  var testConfig = function (configFile, customOptions) {
     var options = { configFile: configFile, singleRun: true };
-    var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: ['dots'] };
+    var travisOptions = process.env.TRAVIS && { browsers: [ 'Firefox', 'PhantomJS'], reporters: ['dots'] };
     return grunt.util._.extend(options, customOptions, travisOptions);
   };
 
+
   // Project configuration.
   grunt.initConfig({
-    dist : 'components/angular-ui-docs',
+    bower: 'bower_components',
+    dist: '<%= bower %>/angular-ui-docs',
     pkg: grunt.file.readJSON('package.json'),
     meta: {
       banner: ['/**',
@@ -27,27 +31,44 @@ module.exports = function (grunt) {
         ' * @license <%= pkg.license %>',
         ' */',
         ''].join('\n'),
-      view : {
-        humaName : "UI Map",
-        repoName : "ui-map"
+      view: {
+        humaName: "UI.Map",
+        repoName: "ui-map",
+        demoHTML: grunt.file.read("demo/demo.html"),
+        demoJS: grunt.file.read("demo/demo.js"),
+        css: [
+          'assets/css/demos.css'
+        ],
+        js: [
+          'https://rawgithub.com/angular-ui/ui-utils/master/modules/event/event.js',
+          'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initCall',
+          'build/ui-map.js'
+        ]
+      }
+    },
+    watch: {
+      karma: {
+        files: ['ui-map.js', 'test/*.js'],
+        tasks: ['karma:unit:run'] //NOTE the :run flag
       }
     },
     karma: {
-      unit: testConfig('test/karma.conf.js')
+      unit: testConfig('test/karma.conf.js'),
+      start: {configFile: 'test/karma.conf.js'}
     },
-    jshint:{
-      files:['<%= meta.view.repoName %>.js', 'gruntFile.js', 'test/**/*Spec.js', 'demo/**/*.js'],
-      options:{
-        curly:true,
-        eqeqeq:true,
-        immed:true,
-        latedef:true,
-        newcap:true,
-        noarg:true,
-        sub:true,
-        boss:true,
-        eqnull:true,
-        globals:{}
+    jshint: {
+      files: ['<%= meta.view.repoName %>.js', 'gruntFile.js', 'test/**/*Spec.js', 'demo/**/*.js'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
       }
     },
     uglify: {
@@ -63,12 +84,11 @@ module.exports = function (grunt) {
         files: [
           {src: ['<%= meta.view.repoName %>.js'], dest: '<%= dist %>/build/<%= meta.view.repoName %>.js', filter: 'isFile'},
           {src: ['demo/demo.html'], dest: '<%= dist %>/demos.html', filter: 'isFile'},
-          {src: ['demo/demo.css'], dest: '<%= dist %>/assets/css/demos.css', filter: 'isFile'},
-          {src: ['demo/demo.js'], dest: '<%= dist %>/core/demo.js', filter: 'isFile'}
+          {src: ['demo/demo.css'], dest: '<%= dist %>/assets/css/demos.css', filter: 'isFile'}
         ]
       },
-      template : {
-        options : {processContent : function(content){
+      template: {
+        options: {processContent: function (content) {
           return grunt.template.process(content);
         }},
         files: [
