e04a6ad2355bb0015a98047aa770607c59037362
Pascal Precht <pascal.precht@googlemail.com>
2013-12-04 23:07:24 +0100

    chore(grunt): adds load-grunt-tasks

--- gruntFile.js
+++ gruntFile.js
@@ -1,11 +1,6 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-conventional-changelog');
+  require('load-grunt-tasks')(grunt);
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'karma:unit']);
