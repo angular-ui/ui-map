76de931064838efd2832bb275d68ce565df4b6b9
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2013-12-28 18:17:37 +0000

    Travis commit : build 40

---
+++ demo/demo.js
@@ -0,0 +1,44 @@
+/* global console:false, google:false */
+/*jshint unused:false */
+'use strict';
+
+function initCall() {
+  console.log('Google maps api initialized.');
+  angular.bootstrap(document.getElementById('map'), ['doc.ui-map']);
+}
+
+angular.module('doc.ui-map', ['ui.map', 'prettifyDirective'])
+  .controller('MapCtrl', ['$scope', function ($scope) {
+
+    $scope.myMarkers = [];
+
+    $scope.mapOptions = {
+      center: new google.maps.LatLng(35.784, -78.670),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.addMarker = function ($event, $params) {
+      $scope.myMarkers.push(new google.maps.Marker({
+        map: $scope.myMap,
+        position: $params[0].latLng
+      }));
+    };
+
+    $scope.setZoomMessage = function (zoom) {
+      $scope.zoomMessage = 'You just zoomed to ' + zoom + '!';
+      console.log(zoom, 'zoomed');
+    };
+
+    $scope.openMarkerInfo = function (marker) {
+      $scope.currentMarker = marker;
+      $scope.currentMarkerLat = marker.getPosition().lat();
+      $scope.currentMarkerLng = marker.getPosition().lng();
+      $scope.myInfoWindow.open($scope.myMap, marker);
+    };
+
+    $scope.setMarkerPosition = function (marker, lat, lng) {
+      marker.setPosition(new google.maps.LatLng(lat, lng));
+    };
+  }])
+;
