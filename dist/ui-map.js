4e62e8209793b3ad44cd59cc2c976b86902629ff
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-02-23 00:22:15 +0000

    Travis commit : build 52

--- dist/ui-map.js
+++ dist/ui-map.js
@@ -1,10 +1,17 @@
 'use strict';
 (function () {
   var app = angular.module('ui.map', ['ui.event']);
+  //Setup map events from a google map object to trigger on a given element too,
+  //then we just use ui-event to catch events from an element
   function bindMapEvents(scope, eventsStr, googleObject, element) {
     angular.forEach(eventsStr.split(' '), function (eventName) {
+      //Prefix all googlemap events with 'map-', so eg 'click'
+      //for the googlemap doesn't interfere with a normal 'click' event
       window.google.maps.event.addListener(googleObject, eventName, function (event) {
         element.triggerHandler('map-' + eventName, event);
+        //We create an $apply if it isn't happening. we need better support for this
+        //We don't want to use timeout because tons of these events fire at once,
+        //and we only need one $apply
         if (!scope.$$phase) {
           scope.$apply();
         }
@@ -23,6 +30,7 @@
           var opts = angular.extend({}, options, scope.$eval(attrs.uiOptions));
           var map = new window.google.maps.Map(elm[0], opts);
           var model = $parse(attrs.uiMap);
+          //Set scope variable for the map
           model.assign(scope, map);
           bindMapEvents(scope, mapEvents, map, elm);
         }
@@ -47,7 +55,12 @@
             model.assign(scope, infoWindow);
           }
           bindMapEvents(scope, infoWindowEvents, infoWindow, elm);
+          /* The info window's contents dont' need to be on the dom anymore,
+           google maps has them stored.  So we just replace the infowindow element
+           with an empty div. (we don't just straight remove it from the dom because
+           straight removing things from the dom can mess up angular) */
           elm.replaceWith('<div></div>');
+          //Decorate infoWindow.open to $compile contents before opening
           var _open = infoWindow.open;
           infoWindow.open = function open(a1, a2, a3, a4, a5, a6) {
             $compile(elm.contents())(scope);
@@ -57,6 +70,12 @@
       };
     }
   ]);
+  /*
+   * Map overlay directives all work the same. Take map marker for example
+   * <ui-map-marker="myMarker"> will $watch 'myMarker' and each time it changes,
+   * it will hook up myMarker's events to the directive dom element.  Then
+   * ui-event will be able to catch all of myMarker's events. Super simple.
+   */
   function mapOverlayDirective(directiveName, events) {
     app.directive(directiveName, [function () {
         return {
